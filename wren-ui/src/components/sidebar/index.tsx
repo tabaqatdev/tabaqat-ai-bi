18c381b7f68142546328719e550a29af73d6fe0d
yharby <me@youssefharby.com>
2025-11-26 22:49:47 +0200

    feat: rebrand Wren AI to Tabaqat, updating branding assets, links, and adding development setup documentation.

--- wren-ui/src/components/sidebar/index.tsx
+++ wren-ui/src/components/sidebar/index.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import { Button } from 'antd';
 import styled from 'styled-components';
 import { Path } from '@/utils/enum';
-import { DiscordIcon, GithubIcon } from '@/utils/icons';
+import { GithubIcon } from '@/utils/icons';
 import SettingOutlined from '@ant-design/icons/SettingOutlined';
 import Home, { Props as HomeSidebarProps } from './Home';
 import Modeling, { Props as ModelingSidebarProps } from './Modeling';
@@ -95,25 +95,13 @@ export default function Sidebar(props: Props) {
         <StyledButton type="text" block>
           <Link
             className="d-flex align-center"
-            href="https://discord.com/invite/5DvshJqG8Z"
+            href="https://tabaqat.net"
             target="_blank"
             rel="noopener noreferrer"
             data-ph-capture="true"
-            data-ph-capture-attribute-name="cta_go_to_discord"
+            data-ph-capture-attribute-name="cta_go_to_tabaqat"
           >
-            <DiscordIcon className="mr-2" style={{ width: 16 }} /> Discord
-          </Link>
-        </StyledButton>
-        <StyledButton type="text" block>
-          <Link
-            className="d-flex align-center"
-            href="https://github.com/Canner/WrenAI"
-            target="_blank"
-            rel="noopener noreferrer"
-            data-ph-capture="true"
-            data-ph-capture-attribute-name="cta_go_to_github"
-          >
-            <GithubIcon className="mr-2" style={{ width: 16 }} /> GitHub
+            <GithubIcon className="mr-2" style={{ width: 16 }} /> Tabaqat
           </Link>
         </StyledButton>
       </div>
